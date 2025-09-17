5f672ab55e5088291ef561661ec3310c4aef9dd2
Crans3 <90653738+Crans3@users.noreply.github.com>
2025-09-17 11:19:21 +0200

    Created blank pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,37 @@
-
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import Homepage from './components/homepage/Homepage'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router'
+import Homepage from './components/homepage/homepage'
+import Login from './components/login/login'
+import Register from './components/register/register'
 
 const router = createBrowserRouter([
-    {
-        path: "/",
+  {
+    path: "/",
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/home" replace />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/register",
+        element: <Register />
+      },
+      {
+        path: "/home",
         element: <Homepage />
-    }
+      },
+    ]
+  }
 ])
 
 
 function App(){
-    return(
-        <RouterProvider router={router} />
-    )
+  return(
+    <RouterProvider router={router} />
+  )
 }
 
 
---
+++ frontend/src/components/register/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import s from './register.module.scss'
+
+const register = () => {
+  return (
+    <div className={s["main-container"]}>
+      Register Works
+    </div>
+  )
+}
+
+export default register
