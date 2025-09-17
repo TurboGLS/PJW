0c3483d25f65e23187b5c112b5853f1d2a506521
TurboGLS <matteazzitomas@gmail.com>
2025-09-17 10:51:21 +0200

    fix + to add user

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-// import apiRouter from './api/routes';
+import apiRouter from './api/routes';
 import bodyParser from 'body-parser';
-// import { errorHandlers } from './errors';
-// import './lib/auth/auth.handlers';
+import { errorHandlers } from './errors';
+import './lib/auth/auth.handlers';
 
 const app = express();
 
@@ -28,8 +28,8 @@ app.use(cors({
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
-// app.use('/api', apiRouter);
+app.use('/api', apiRouter);
 
-// app.use(errorHandlers);
+app.use(errorHandlers);
 
 export default app;
--- backend/src/lib/auth/jwt/jwt-strategy.ts
+++ backend/src/lib/auth/jwt/jwt-strategy.ts
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { ExtractJwt, Strategy as JwtStrategy } from 'passport-jwt';
-    import { UserModel } from "../../../api/user/user.model";
+import { UserModel } from "../../../api/user/user.model";
 
 export const JWT_SECRET = 'my_jwt_secret';
 
@@ -9,17 +9,17 @@ passport.use(
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
         secretOrKey: JWT_SECRET
     },
-    async (token, done) => {
-        try {
-            const user = await UserModel.findById(token.id);
-            if (user) {
-                done(null, user.toObject());
-            } 
-            else {
-                done(null, false, { message: 'invalid token'});
+        async (token, done) => {
+            try {
+                const user = await UserModel.findById(token.id);
+                if (user) {
+                    done(null, user.toObject());
+                }
+                else {
+                    done(null, false, { message: 'invalid token' });
+                }
+            } catch (err) {
+                done(err);
             }
-        } catch(err) {
-            done(err);
-        }
-    })
+        })
 );
