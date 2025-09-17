830ee967d6f67734ec30289fa8446ef48b7019e8
TurboGLS <matteazzitomas@gmail.com>
2025-09-17 14:22:58 +0200

    authRouter

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import authRouter from './auth/auth.router';
 import userRouter from './user/user.router';
 
 const router = Router();
 
+router.use(authRouter);
 router.use('/user', userRouter);
 
 export default router;
