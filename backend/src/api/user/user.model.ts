c8bc546929fee875cd08e6aa125d595751d9b730
TurboGLS <matteazzitomas@gmail.com>
2025-09-17 11:17:40 +0200

    user entity + model

---
+++ backend/src/api/user/user.model.ts
@@ -0,0 +1,30 @@
+import { model, Schema } from "mongoose";
+import { User } from "./user.entity";
+
+const userSchema = new Schema<User>({
+    firstName: { type: String },
+    lastName: { type: String },
+    username: { type: String },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+});
+
+userSchema.set('toJSON', {
+    virtuals: true,
+    transform: (_, ret: any) => {
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+    }
+});
+
+userSchema.set('toObject', {
+    virtuals: true,
+    transform: (_, ret: any) => {
+        delete ret._id;
+        delete ret.__v;
+        return ret;
+    }
+});
+
+export const UserModel = model<User>('User', userSchema);
