ca54cc213d4e338d246338a35a1e33614f99ca9a
TurboGLS <matteazzitomas@gmail.com>
2025-09-18 08:51:52 +0200

    fixed verification email

--- backend/src/api/routes.ts
+++ backend/src/api/routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRouter from './auth/auth.router';
 import userRouter from './user/user.router';
 import categoryRouter from './category/category.router';
+import verificationRouter from './verification/verification.router';
 
 const router = Router();
 
 router.use(authRouter);
 router.use('/user', userRouter);
 router.use('/categoryTypes', categoryRouter);
+router.use('/verification', verificationRouter);
 
 export default router;
