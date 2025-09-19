3a7787e04813a4471bb58fb7dce3b30920a4dfa8
Crans3 <90653738+Crans3@users.noreply.github.com>
2025-09-19 17:14:12 +0200

    Added hamburgen menu on sidebar

--- frontend/src/components/sidebar/sidebar.tsx
+++ frontend/src/components/sidebar/sidebar.tsx
@@ -5,6 +5,7 @@ import authService from "../../services/auth.service";
 import type { User } from "../../entities/user.entity";
 import logo from "../../assets/intesa-mario-volpato-trasparent.png"
 import Icon from '@mdi/react';
+import { mdiMenu } from '@mdi/js';
 import { mdiBank } from '@mdi/js';
 import { mdiCellphoneArrowDownVariant } from '@mdi/js';
 import { mdiCashMultiple } from '@mdi/js';
@@ -12,6 +13,7 @@ import { useNavigate } from "react-router";
 
 const Sidebar = () => {
   const [user, setUser] = useState<User | null>();
+  const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
 
   const fetchUser = async () => {
@@ -32,9 +34,16 @@ const Sidebar = () => {
     navigate("/profile");
   }
 
+  const toggleSidebar = () => {
+    setCollapsed((prev) => !prev);
+  }
+
   return (
-    <div className={s["main-container"]}>
-      <img className={s["logo"]} src={logo} />
+    <div className={`${s["main-container"]} ${collapsed ? s["collapsed"] : ""}`}>
+      <div className={s["logo-container"]}>
+        <div onClick={toggleSidebar}><Icon path={mdiMenu} size={1} /></div>
+        <img className={s["logo"]} src={logo} />
+      </div>
       <div className={s["page-button"]}>
         <Icon path={mdiBank} size={1} />
         <label>Bonifico</label>
@@ -47,9 +56,14 @@ const Sidebar = () => {
         <Icon path={mdiCashMultiple} size={1} />
         <label>Saldo</label>
       </div>
-      <div className={s["user-area"]} onClick={redirectToProfile}>
-        <ProfileInfos user={user}></ProfileInfos>
-      </div>
+      { 
+        !collapsed ? 
+          <div className={s["user-area"]} onClick={redirectToProfile}>
+            <ProfileInfos user={user}></ProfileInfos>
+          </div>
+        :
+          <img onClick={redirectToProfile} src="https://cdn-icons-png.freepik.com/256/12225/12225881.png?semt=ais_white_label"/>
+      }
     </div>
   );
 };
