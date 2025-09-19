e7f46b81a9602e52e992b859cd9ed2d49eb7ff95
EttoreCisco <ciscoettore2002@gmail.com>
2025-09-19 17:08:08 +0200

    add bank-account infos

--- frontend/src/components/bank-account-infos/bank-account-infos.tsx
+++ frontend/src/components/bank-account-infos/bank-account-infos.tsx
@@ -1,17 +1,18 @@
 import type { BankAccount } from "../../entities/bank-account";
 
+import s from "./bank-account-infos.module.scss";
+
 interface BankAccountInfosProps {
   bankaccountInfos: BankAccount;
 }
 
 const BankAccountInfos = ({ bankaccountInfos }: BankAccountInfosProps) => {
   return (
-    <div>
+    <div className={s["main-container"]}>
       <div>{bankaccountInfos.nomeTitolare}</div>
       <div>{bankaccountInfos.cognomeTitolare}</div>
       <div>{bankaccountInfos.email}</div>
       <div>{bankaccountInfos.iban}</div>
-      <div>{bankaccountInfos.dataApertura.toString()}</div>
     </div>
   );
 };
