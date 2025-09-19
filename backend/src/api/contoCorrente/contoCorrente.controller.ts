7a9ff66e154b853ff60ae10600b41ed4eb25f2db
TurboGLS <matteazzitomas@gmail.com>
2025-09-19 12:12:13 +0200

    myInfoContoCorrente works

--- backend/src/api/contoCorrente/contoCorrente.controller.ts
+++ backend/src/api/contoCorrente/contoCorrente.controller.ts
@@ -1,8 +1,28 @@
-import { Request, Response } from 'express';
-import { ContoCorrenteService } from './contoCorrente.service';
+import { NextFunction, Request, Response } from 'express';
+import contoCorrenteSrv from './contoCorrente.service';
 
-const contoCorrenteService = new ContoCorrenteService();
+export const findMyContoCorrente = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const email = req.user?.email;
 
-export class contoCorrenteController {
+        if (!email) {
+            res.status(400).json({ message: 'Email non trovato' });
+            return;
+        }
 
+        const myInfo = await contoCorrenteSrv.getContoCorrenteByEmail(email);
+
+        if (!myInfo) {
+            res.status(400).json({ message: `Informazioni per l'utente con email: ${email} non trovate` });
+            return;
+        }
+
+        res.status(200).json(myInfo);
+    } catch (err) {
+        next(err);
+    }
 }
