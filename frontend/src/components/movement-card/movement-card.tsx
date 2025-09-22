f19521ada25466a843da21c6256d1d4e6cf87483
ettig <ciscoettore2002@gmail.com>
2025-09-22 22:16:30 +0200

    change style & logic

--- frontend/src/components/movement-card/movement-card.tsx
+++ frontend/src/components/movement-card/movement-card.tsx
@@ -1,20 +1,30 @@
+import { mdiDotsHorizontalCircleOutline } from "@mdi/js";
 import Icon from "@mdi/react";
 import type { Movements } from "../../entities/movements.entity";
 import s from "./movement-card.module.scss";
-import { mdiBankTransfer } from "@mdi/js";
 
 interface MovementCardProps {
   movement: Movements;
 }
 
 const MovementCard = ({ movement }: MovementCardProps) => {
+  const bonificoInUscitaIMG = "Bonifico in uscita";
   return (
     <div className={s["main-container"]}>
-      <div className={s["main-container__top"]}>
-        <Icon path={mdiBankTransfer} size={1} />
+      <div className={s["main-container__wrap"]}>
+        {movement.descrizioneEstesa === bonificoInUscitaIMG ? (
+          <div>Ciao</div>
+        ) : (
+          <div>papere</div>
+        )}
+        <div>{movement.descrizioneEstesa}</div>
+        <div>{movement.importo}$</div>
       </div>
-      <div>{movement.categoriaMovimentoID.categoryName}</div>
-      <div>{movement.importo}$</div>
+      <div className={s["main-container__details-button"]}>
+        <button>
+          <Icon path={mdiDotsHorizontalCircleOutline} size={1} />
+        </button>
+      </div>
     </div>
   );
 };
