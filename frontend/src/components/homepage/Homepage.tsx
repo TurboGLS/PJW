b8aa41dbeed14f784b1f5e18fd67214bb92a3f04
ettig <ciscoettore2002@gmail.com>
2025-09-22 17:38:03 +0200

    add fetch function and new components

--- frontend/src/components/homepage/Homepage.tsx
+++ frontend/src/components/homepage/Homepage.tsx
@@ -3,19 +3,32 @@ import balanceService from "../../services/balance.service";
 import Sidebar from "../sidebar/sidebar";
 import s from "./homepage.module.scss";
 import TotalBalance from "../total-balance/total-balance";
+import MovementsList from "../movements-list/movements-list";
+import movementService from "../../services/movements.service";
+import type { Movements } from "../../entities/movements.entity";
 
 const homepage = () => {
   const [balance, setBalance] = useState();
+  const [movements, setMovements] = useState<Movements[]>();
   const fetchTotalBalance = async () => {
     const response = await balanceService.fetchCurrentBalance();
     setBalance(response.data);
     return response;
   };
+  const fetchMovements = async () => {
+    const response = await movementService.fetchLimitedMovements();
+    setMovements(response.data.movimenti);
+    console.log(movements);
+  };
   useEffect(() => {
     fetchTotalBalance();
+    fetchMovements();
   }, [TotalBalance]);
 
   if (!balance) {
+    return;
+  }
+  if (!movements) {
     return;
   }
   return (
@@ -27,6 +40,8 @@ const homepage = () => {
         <div className={s["main-container__home__infos"]}>
           <TotalBalance totalBalance={balance}></TotalBalance>
         </div>
+        <h3>I TUOI MOVIMENTI</h3>
+        <MovementsList movements={movements}></MovementsList>
       </div>
     </div>
   );
