52a496df0715411c3ebdd551d4799eacd5375196
EttoreCisco <ciscoettore2002@gmail.com>
2025-09-25 12:44:55 +0200

    add loader to login

--- frontend/src/components/login/login.tsx
+++ frontend/src/components/login/login.tsx
@@ -3,12 +3,14 @@ import logo from "../../assets/intesa-mario-volpato-trasparent.png";
 import { useNavigate } from "react-router";
 import { useState, useEffect } from "react";
 import { useAuth } from "../../contexts/auth.context";
+import loaderGif from "../../assets/1474.gif";
 
 const Login = () => {
   const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [isloading, setLoading] = useState(false);
   const { login } = useAuth();
 
   useEffect(() => {
@@ -30,44 +32,57 @@ const Login = () => {
   const handleLogin = async (e: any) => {
     e.preventDefault();
     try {
+      setLoading(true);
       await login(username, password);
       navigate("/homepage");
     } catch (e: any) {
+      setLoading(false);
       setError(e.response.data.message);
     }
   };
 
   return (
-    <div className={s["main-container"]}>
-      <div className={s["login-container"]}>
-        <div className={s["login-area"]}>
-          <div className={s["main-title"]}>Login</div>
-          <div className={s["register-button"]} onClick={redirectToRegister}>
-            If you don't have an account, Click here
-            <hr />
-          </div>
-          <form onSubmit={handleLogin} className={s["form-container"]}>
-            <input
-              className={s["form-button"]}
-              type="email"
-              name="Email"
-              placeholder="Email"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              className={s["form-button"]}
-              type="password"
-              name="Password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            {error ? <p>{error}</p> : null}
-            <input className={s["login-button"]} type="submit" value="Login" />
-          </form>
+    <div>
+       {isloading ? (
+        <div className={s["loader"]}>
+          <img src={loaderGif}></img>
         </div>
-        <img className={s["image"]} src={logo} alt="Logo" />
+      ) : null}
+      <div className={s["main-container"]}>
+        <div className={s["login-container"]}>
+          <div className={s["login-area"]}>
+            <div className={s["main-title"]}>Login</div>
+            <div className={s["register-button"]} onClick={redirectToRegister}>
+              If you don't have an account, Click here
+              <hr />
+            </div>
+            <form onSubmit={handleLogin} className={s["form-container"]}>
+              <input
+                className={s["form-button"]}
+                type="email"
+                name="Email"
+                placeholder="Email"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <input
+                className={s["form-button"]}
+                type="password"
+                name="Password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              {error ? <p>{error}</p> : null}
+              <input
+                className={s["login-button"]}
+                type="submit"
+                value="Login"
+              />
+            </form>
+          </div>
+          <img className={s["image"]} src={logo} alt="Logo" />
+        </div>
       </div>
     </div>
   );
